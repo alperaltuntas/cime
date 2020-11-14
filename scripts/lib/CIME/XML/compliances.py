db880be9d933fb5f495b57f597a9db713cc5c266
alperaltuntas <alperaltuntas@gmail.com>
2020-11-14 14:35:55 -0700

    rename depends as compliances

--- scripts/lib/CIME/XML/compliances.py
+++ scripts/lib/CIME/XML/compliances.py
@@ -1,5 +1,5 @@
 """
-Common interface to XML files which follow the depends format.
+Common interface to XML files which follow the comply format.
 This class inherits from the abstract class GenericXML.
 """
 
@@ -9,15 +9,15 @@ from CIME.XML.generic_xml import GenericXML
 
 logger = logging.getLogger(__name__)
 
-class Depends(GenericXML):
+class Compliances(GenericXML):
 
     def __init__(self, infile=None, files=None):
         if files is None:
             files = Files()
         if infile is None:
-            infile = files.get_value("DEPENDS_SPEC_FILE")
-        logger.debug("Dependencies specification files is {}".format(infile))
-        schema = files.get_schema("DEPENDS_SPEC_FILE")
+            infile = files.get_value("COMPLY_SPEC_FILE")
+        logger.debug("Compliances specification files is {}".format(infile))
+        schema = files.get_schema("COMPLY_SPEC_FILE")
         GenericXML.__init__(self,infile,schema)
         #try:
         #    GenericXML.__init__(self,infile,schema)
@@ -25,5 +25,5 @@ class Depends(GenericXML):
         #    expect(False, "Could not initialize Grids")
         self._version = self.get_version()
 
-        print("Constructed Depends.")
+        print("Constructed Comply.")
 
